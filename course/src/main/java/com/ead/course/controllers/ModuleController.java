3b9b504a18a7f780d88d618fe65c514e71a6b8f4
Aline Paz <aline.soares.2@hotmail.com>
2023-03-29 14:01:21 -0300

    metodo getOneModule

--- course/src/main/java/com/ead/course/controllers/ModuleController.java
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -74,4 +74,15 @@ public class ModuleController {
 
         return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(courseId));
     }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object>getOneModule(@PathVariable(value = "courseId")UUID courseId,
+                                              @PathVariable(value = "moduleId")UUID moduleId){
+
+        Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (!moduleModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModelOptional.get());
+    }
 }
