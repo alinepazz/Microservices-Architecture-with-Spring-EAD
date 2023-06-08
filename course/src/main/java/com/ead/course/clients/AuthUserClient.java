67bf849637812373359fbc16db1fac31c1d270ac
Aline Paz <aline.soares.2@hotmail.com>
2023-06-08 09:27:46 -0300

    deleção em cascata entre microservices

--- course/src/main/java/com/ead/course/clients/AuthUserClient.java
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -64,4 +64,9 @@ public class AuthUserClient {
         courseUserDto.setUserId(userId);
         restTemplate.postForObject(url, courseUserDto, String.class);
     }
+
+    public void deleteCourseInAuthUser(UUID courseId) {
+        String url = REQUEST_URL_AUTHUSER + "/users/courses/" + courseId;
+        restTemplate.exchange(url, HttpMethod.DELETE, null, String.class);
+    }
 }
